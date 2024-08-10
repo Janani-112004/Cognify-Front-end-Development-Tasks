ab4eceb9fe71fed2470f82e25cdb4e577be71ad6
Janani-112004 <164316229+Janani-112004@users.noreply.github.com>
2024-08-10 12:18:01 +0530

    Add files via upload

---
+++ Front-end Development/Task 5/app.js
@@ -0,0 +1,19 @@
+async function getData(){
+    const data = await fetch('https://dummyjson.com/users');
+    const records = await data.json();
+
+    let tab ='';
+    records.users.forEach(function(user){
+        tab +=`<tr>
+        <td>${user.Firstname}</td> 
+        <td>${user.lastname}</td>
+        <td>${user.age}</td>
+        <td>${user.email}</td>
+        <td>${user.gender}</td>
+        </tr>`
+    
+    })
+    document.getElementById('tbody').innerHTML= tab;
+
+
+}
---
+++ Front-end Development/Task4/app.js
@@ -0,0 +1,13 @@
+const name = document.querySelector(".name");
+
+function changeColor(color) {
+   name.style.color = color;
+}
+function changeColor(color) {
+    document.body.style.background = color;
+}
+
+function myFunc() {
+    changeColor('wheat');
+    result.innerHTML = "Background Color changed";
+}        
